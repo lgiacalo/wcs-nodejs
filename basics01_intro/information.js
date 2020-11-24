08b3f63f07c190a601c466be78c565fa342dc79d
lgiacalo <lgiacalo@student.42.fr>
2020-11-24 11:54:42 +0100

    reorga

---
+++ basics01_intro/information.js
@@ -0,0 +1,11 @@
+let name = "John Doe";
+let age = 26;
+let passions = ["Ski", "Hiking", "Cinema"];
+
+const myInfos = () => {
+  console.log(
+    `Hi my name is ${name}, I'm ${age} and my passions are ${passions}`
+  );
+};
+
+module.exports = { myInfos };
